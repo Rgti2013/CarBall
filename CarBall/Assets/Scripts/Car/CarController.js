083db5d9d5dec816f11387563d2de98288fe9f7f
okacat <oskar.korosec@gmail.com>
2013-12-24 00:09:52 +0100

    First commit
    - basic field
    - basic car

---
+++ CarBall/Assets/Scripts/Car/CarController.js
@@ -0,0 +1,74 @@
+﻿//CarController1.js
+
+/*
+ TODO: 
+	 - Lower the tire friction at high speed to prevent the car flipping
+	 - Control the visual representation of the wheels
+	 - Adjust the friction on the wheels manually to prevent flipping
+*/  
+
+var wheels : Transform[];
+
+var enginePower=150.0;
+
+private var power=0.0;
+private var brake=0.0;
+private var steer=0.0;
+
+private var maxSteer= 35.0;
+
+
+function Start(){
+    rigidbody.centerOfMass=Vector3(0,-0.5,0.3);
+}
+
+function Update () {
+
+	// Power the vehicle
+    power=Input.GetAxis("Vertical") * enginePower * Time.deltaTime * 250.0;
+    
+    // Steer the vehicle
+    steer=Input.GetAxis("Horizontal") * maxSteer;
+    
+    // Apply brakes
+    brake=Input.GetKey("space") ? rigidbody.mass * 0.1: 0.0;
+
+    
+    GetCollider(0).steerAngle=steer;
+    GetCollider(1).steerAngle=steer;
+
+    if(brake > 0.0){
+
+        GetCollider(0).brakeTorque=brake;
+        GetCollider(1).brakeTorque=brake;
+        GetCollider(2).brakeTorque=brake;
+        GetCollider(3).brakeTorque=brake;
+        GetCollider(2).motorTorque=0.0;
+        GetCollider(3).motorTorque=0.0;
+
+    } else {
+
+        GetCollider(0).brakeTorque=0;
+        GetCollider(1).brakeTorque=0;
+        GetCollider(2).brakeTorque=0;
+        GetCollider(3).brakeTorque=0;
+        GetCollider(2).motorTorque=power;
+        GetCollider(3).motorTorque=power;
+
+    }
+    
+    // Handbrakes
+    if(Input.GetKey(KeyCode.Space)) {
+    	GetCollider(2).brakeTorque=1000f;
+    	GetCollider(3).brakeTorque=1000f;
+    }
+
+}
+
+ 
+
+function GetCollider(n : int) : WheelCollider{
+
+    return wheels[n].gameObject.GetComponent(WheelCollider);
+
+}
