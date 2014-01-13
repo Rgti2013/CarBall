6cb193e8df627da4de0710d843b92b4872ecb649
nejc92 <nejc.il92@gmail.com>
2014-01-13 01:27:50 +0100

    firing catcher + pickups

---
+++ CarBall/Assets/Scripts/Car/FireCatcher.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+
+private var configurableJoint : ConfigurableJoint;
+public var gamePlayLogic : GamePlayLogic;
+private var power : Vector3;
+public var playerId : int;
+
+function Start () {
+
+}
+
+function Update () {
+
+	if(playerId == 1){
+
+		if(Input.GetKeyDown(KeyCode.N)){
+			
+			power = gamePlayLogic.player1Power;
+			configurableJoint = GetComponent(ConfigurableJoint);
+			configurableJoint.targetVelocity = power;
+		}
+		else if(Input.GetKeyUp(KeyCode.N)){
+
+			configurableJoint = GetComponent(ConfigurableJoint);
+			configurableJoint.targetVelocity = Vector3(0, 0, -2);
+		}
+	}
+	else if(playerId == 2){
+
+		if(Input.GetKeyDown(KeyCode.G)){
+			
+			power = gamePlayLogic.player2Power;
+			configurableJoint = GetComponent(ConfigurableJoint);
+			configurableJoint.targetVelocity = power;
+		}
+		else if(Input.GetKeyUp(KeyCode.G)){
+
+			configurableJoint = GetComponent(ConfigurableJoint);
+			configurableJoint.targetVelocity = Vector3(0, 0, -2);
+		}
+	}
+}
---
+++ CarBall/Assets/Scripts/Pickups/PickupTriggerMagnet.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+public var gamePlayLogic : GamePlayLogic;
+private var carName : String;
+
+function OnTriggerEnter (other : Collider) {
+
+	carName = other.transform.parent.gameObject.name;
+	if(other.tag == "car") {
+
+		if(carName == "Beetle1") {
+			gamePlayLogic.addPower(1, Vector3(0, 0, 5));
+		} else if(carName == "Beetle2") {
+			gamePlayLogic.addPower(2, Vector3(0, 0, 5));
+		}
+	}
+	Destroy(transform.root.gameObject);
+}
